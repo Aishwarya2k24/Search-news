eea2667ed0d1d5c8e4396d9b4a80b860e24a2221
Aishwarya2k24 <kurumkaraishwarya27@gmail.com>
2024-05-19 18:42:53 +0530

    updated

---
+++ script.js
@@ -0,0 +1,50 @@
+const apiKey =
+"e2880514665d431eb47664e137ea68fa";
+
+const blogContainer = document.getElementById
+("blog-container");
+ async function fetchRandomNews()
+ {
+    try{
+        const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&pageSize=10&apiKey=${apiKey}`;
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        return data.articles;
+    
+    } catch(error){
+         console.error("Error fetching random news",error);
+         return [];
+    }
+
+ }
+ function  displayBlogs(articles){
+    blogContainer.innerHTML = "";
+    articles.forEachar((article) =>{
+        const blogCard = document.createElement
+        ("div");
+        blogCard.classList.add("blog-card");
+        const img = document.createElement("img");
+        img.src =  article.urlToImage;
+        img.alt = article.title;
+        const  title = document.createElement("h2");
+        title.textContent = article.title;
+        const description = document.createElement("p");
+        description.textContent = article.description;
+
+        blogCard.appendChild(img);
+        blogCard.appendChild(title);
+        blogCard.appendChild(description);
+        blogContainer.appendChild(blogCard);
+
+    })
+
+ }
+
+ (async () => {
+   try {
+    const articles = await fetchRandomNews();
+    displayBlogs(articles);
+  }  catch(error){
+      console.error("Error fetching random news",error);
+   }
+ })();
